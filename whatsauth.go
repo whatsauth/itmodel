733a442b007fa1ba401104a63045a212b58878fa
Rolly Maulana Awangga <awangga@gmail.com>
2024-07-20 08:21:10 +0700

    tambah apidocs sama image

--- whatsauth.go
+++ whatsauth.go
@@ -31,6 +31,8 @@ type Profile struct {
 	Secret        string `bson:"secret"`
 	URL           string `bson:"url"`
 	URLAPIText    string `bson:"urlapitext"`
+	URLAPIImage   string `bson:"urlapiimage"`
+	URLAPIDoc     string `bson:"urlapidoc"`
 	QRKeyword     string `bson:"qrkeyword"`
 	Botname       string `bson:"botname"`
 	Triggerword   string `bson:"triggerword"`
